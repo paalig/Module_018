2284dba5b57535fb2a169922c047f8f246201c17
Alex <alex0692pash@mail.ru>
2020-11-30 23:27:00 +0300

    Init 001

---
+++ 001_MaxDelta/cmake-build-debug/CMakeFiles/001_MaxDelta.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "001_MaxDelta.exe"
+  "001_MaxDelta.exe.manifest"
+  "001_MaxDelta.pdb"
+  "CMakeFiles/001_MaxDelta.dir/main.cpp.obj"
+  "lib001_MaxDelta.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/001_MaxDelta.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
