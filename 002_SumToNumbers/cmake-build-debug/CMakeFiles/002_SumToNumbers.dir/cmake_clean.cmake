af8625d06e1f6e449abb7f372c66a643e9f4b105
Alex <alex0692pash@mail.ru>
2020-12-01 00:55:17 +0300

    Init 002

---
+++ 002_SumToNumbers/cmake-build-debug/CMakeFiles/002_SumToNumbers.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "002_SumToNumbers.exe"
+  "002_SumToNumbers.exe.manifest"
+  "002_SumToNumbers.pdb"
+  "CMakeFiles/002_SumToNumbers.dir/main.cpp.obj"
+  "lib002_SumToNumbers.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/002_SumToNumbers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
