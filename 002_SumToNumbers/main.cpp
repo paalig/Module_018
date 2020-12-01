05329fe529e96931188b3ea227bc0c19234c5afd
Alex <alex0692pash@mail.ru>
2020-12-01 23:22:01 +0300

    002 done

--- 002_SumToNumbers/main.cpp
+++ 002_SumToNumbers/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 /*
  * Вам даётся массив целых чисел и одно число -- результат.
  * Необходимо найти в массиве 2 числа, сумма которых будет давать результат, и вывести их на экран.
@@ -8,6 +9,35 @@
  */
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::vector<int> vec = {15, 11, 2, 7};
+    int result = 26;
+
+    for (int i = 0; i < vec.size(); i++) {
+        for (int j = 0; j < vec.size() - i - 1; j++) {
+            if (vec[j] > vec[j + 1]) {
+                int temp = vec[j];
+                vec[j] = vec[j + 1];
+                vec[j + 1] = temp;
+            }
+        }
+    }
+
+    bool checkResult = false;
+    int a;
+    int b;
+    for (int i = 0; i < vec.size() && !checkResult; i++) {
+        for (int j = i + 1; j < vec.size() && !checkResult; j++) {
+            if (vec[i] + vec[j] == result) {
+                a = vec[i];
+                b = vec[j];
+                checkResult = true;
+            }
+        }
+    }
+
+    if (checkResult) {
+        std::cout << a << " " << b << std::endl;
+    } else {
+        std::cout << "There is no required result in array of numbers";
+    }
 }
