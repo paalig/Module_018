581dd8be12ad9c4385d964d459778f79051032de
Alex <alex0692pash@mail.ru>
2020-12-01 23:27:48 +0300

    Init 003

---
+++ 003_FifthNumberAsc/cmake-build-debug/CMakeFiles/003_FifthNumberAsc.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "003_FifthNumberAsc.exe"
+  "003_FifthNumberAsc.exe.manifest"
+  "003_FifthNumberAsc.pdb"
+  "CMakeFiles/003_FifthNumberAsc.dir/main.cpp.obj"
+  "lib003_FifthNumberAsc.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/003_FifthNumberAsc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
