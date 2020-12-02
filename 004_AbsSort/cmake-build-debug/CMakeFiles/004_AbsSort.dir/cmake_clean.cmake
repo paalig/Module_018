5f744fd9ea5226ed6c6325cab598a96d6659b948
Alex <alex0692pash@mail.ru>
2020-12-02 18:40:54 +0300

    Init 004

---
+++ 004_AbsSort/cmake-build-debug/CMakeFiles/004_AbsSort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "004_AbsSort.exe"
+  "004_AbsSort.exe.manifest"
+  "004_AbsSort.pdb"
+  "CMakeFiles/004_AbsSort.dir/main.cpp.obj"
+  "lib004_AbsSort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/004_AbsSort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
